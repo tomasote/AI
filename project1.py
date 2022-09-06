8491bde63ece2843015a89a18390f824606bbca2
Tomas Pérez <poatati@hotmail.com>
2022-09-06 12:09:46 +0200

    minor changes

--- project1.py
+++ project1.py
@@ -5,7 +5,6 @@ import time
 
 
 class Node:
-    #Might not use
     def __init__(self, cost, state, f):
         self.cost = cost
         self.state = state
@@ -30,7 +29,7 @@ FAILURE = "There is no path"
 frontier = []
 reached = []
 df = pd.read_csv('100_nodes.csv')
-w = 0
+w = 0.5
 start_node_idx = 0
 end_node_idx = 19
 path = []
@@ -55,21 +54,11 @@ def get_possible_directions(df, node_idx):
 
 def generate_children(df, w, lst, end_node_idx, parent):
     result = []
-    #parent.update_path(parent.state)
-    #print(lst)
     for i in lst:
-        #print(i+3)
         cost = df.iloc[parent.state][i+3]
-        #print(cost)
-        
-        #print(parent.cost)
-        #print(parent.path)
-        #print(parent.state)
-        #print(f"parent: {parent.path}")
         node = Node(cost+parent.cost, i, f(df, w, i, end_node_idx))
         node.set_f(f(df, w, i, end_node_idx))
         node.set_path(parent.path + [i])
-        #print(f"Child: {node.path}")
         result.append(node)
         generated_nodes.append(node)
     return result
@@ -87,7 +76,6 @@ def select_best_node(df, w):
             fbest = f(df, w, node.state, end_node_idx)
             ibest = idx
     node = frontier[ibest]
-    #print(ibest)
     del frontier[ibest]
     return node
     
@@ -100,13 +88,7 @@ def astar(df, w, start_node_idx, end_node_idx):
     reached.append(node)
     while len(frontier) > 0:
         node = select_best_node(df, w)
-        #print(node)
-        #print(get_possible_directions(df, node.state))
-        #print(frontier)
-        #time.sleep(10)
         if node.state == end_node_idx:
-            #node.update_path(node.state)
-            #path.append(node.state)
             return node
         child_list = generate_children(df, w, get_possible_directions(df, node.state), end_node_idx, node)
         for child in child_list:
@@ -119,11 +101,5 @@ def astar(df, w, start_node_idx, end_node_idx):
                     frontier.append(child)
     return FAILURE
     
-#print(df)
-#print(h(df, 1, 2))
-#print(get_possible_directions(df, 0))
-#print(cost(df, 0, 1))
-#print(f(df, 0.5, 99, 99))
 print(astar(df, w, start_node_idx, end_node_idx))
 print(f"Generated nodes: {len(generated_nodes)}")
-#print(df.iloc[25][18])
